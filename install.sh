4f178652f1b111960db9e3a2fa5a17698b77257f
devlulcas <lucasalvesrego.contato@gmail.com>
2022-03-03 22:51:58 -0300

    add: pretty print

--- install.sh
+++ install.sh
@@ -36,6 +36,9 @@ makepkg -si
 
 center_text "${YELLOW} OH MY ZSH ${NOCOLOR}" "=" 5
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 
 center_text "${YELLOW} VIM ${NOCOLOR}" "=" 5
 sudo pacman -S nvim 
--- vscodeext.sh
+++ vscodeext.sh
@@ -1,39 +1,62 @@
 #!/bin/zsh
 
+CYAN='\033[0;36m'
+NOCOLOR='\033[0m'
+
+# shellcheck disable=SC3000-SC4000
+source center.sh
+
 # THEMES
 # omni owl - material icon theme
-code --install-extension guilhermerodz.omni-owl
+center_text "${CYAN} THEMES ${NOCOLOR}" "=" 5
+code --install-extension rocketseat.theme-omni
 code --install-extension PKief.material-icon-theme
 
 # PORTUGUESE
 # language pack ptbr - spell checker portuguese
+center_text "${CYAN} LANGUAGE ${NOCOLOR}" "=" 5
 code --install-extension MS-CEINTL.vscode-language-pack-pt-BR
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension streetsidesoftware.code-spell-checker-portuguese-brazilian
 
 # LANGUAGES / LIBS / FRAMEWORKS
 # react js snippets - svelte - html css - intelephense - phpdebug - python - styled components - svg - md - xml - regex
-code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension svelte.svelte-vscode
-code --install-extension ecmel.vscode-html-css 
-code --install-extension bmewburn.vscode-intelephense-client
-code --install-extension xdebug.php-debug
-code --install-extension ms-python.python
-code --install-extension styled-components.vscode-styled-components
+center_text "${CYAN} BASIC WEB ${NOCOLOR}" "=" 5
 code --install-extension jock.svg
+code --install-extension ecmel.vscode-html-css 
+
+center_text "${CYAN} PYTHON ${NOCOLOR}" "=" 5
+code --install-extension ms-python.python
+code --install-extension esbenp.prettier-vscode
+
+center_text "${CYAN} REACT ${NOCOLOR}" "=" 5
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension styled-components.vscode-styled-components
+
+center_text "${CYAN} SVELTE ${NOCOLOR}" "=" 5
+code --install-extension svelte.svelte-vscode
+
+center_text "${CYAN} PHP ${NOCOLOR}" "=" 5
+code --install-extension xdebug.php-debug
+code --install-extension bmewburn.vscode-intelephense-client
+
+center_text "${CYAN} MARKDOWN ${NOCOLOR}" "=" 5
 code --install-extension bierner.markdown-preview-github-styles
-code --install-extension redhat.vscode-xml
-code --install-extension chrmarti.regex
 
 # NICE
 # live server - intellicode - gitlens - eslint
+center_text "${CYAN} UTILITY ${NOCOLOR}" "=" 5
 code --install-extension ritwickdey.LiveServer
-code --install-extension VisualStudioExptTeam.vscodeintellicode
-code --install-extension eamodio.gitlens
-code --install-extension dbaeumer.vscode-eslint
-
-# OTHER
 # better comments - color highlight - path intellisense - 
 code --install-extension aaron-bond.better-comments
 code --install-extension naumovs.color-highlight
 code --install-extension christian-kohler.path-intellisense
+
+center_text "${CYAN} GIT ${NOCOLOR}" "=" 5
+code --install-extension eamodio.gitlens
+
+# OTHER
+center_text "${CYAN} OTHER ${NOCOLOR}" "=" 5
+code --install-extension redhat.vscode-xml
+code --install-extension chrmarti.regex
+code --install-extension VisualStudioExptTeam.vscodeintellicode
